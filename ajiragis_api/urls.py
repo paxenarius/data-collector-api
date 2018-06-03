fa954091c0fc337512510a3ac3fda291554f5874
steve <steviewez@gmail.com>
2018-06-03 08:13:21 +0300

    integrate theme to dashboard

--- ajiragis_api/urls.py
+++ ajiragis_api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dashboard.views import DashboardTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', DashboardTemplateView.as_view(), name='dashboard')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class DashboardTemplateView(TemplateView):
+    template_name = 'dashboard/dashboard.html'
