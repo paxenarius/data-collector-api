e6ed40db68d908f56124768299f85ed9e8c2d440
steve <steviewez@gmail.com>
2018-06-08 12:25:04 +0300

    update dashboard redirect

--- ajiragis_api/urls.py
+++ ajiragis_api/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from dashboard.views import DashboardTemplateView, SocialLoginTemplateView, PrivacyPolicyTemplateView
-from contribution.views import ContributionListView, ContributionCreateView
 
 urlpatterns = [
     path('', SocialLoginTemplateView.as_view(), name='social'),
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 
 class DashboardTemplateView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('login')
     template_name = 'dashboard/dashboard.html'
 
 
